3f7eed0e3a95ac6c418e7cb45176704b2968d5b4
mnubhan <76425959+mnubhan@users.noreply.github.com>
2023-01-20 00:58:26 +0000

    Create answer.js

---
+++ answer.js
@@ -0,0 +1,9 @@
+const setAlarm = (employed, vacation) => employed && !vacation;
+
+function setAlarm(employed, vacation){
+  return employed && !vacation;
+}
+
+function setAlarm(employed, vacation){
+return (employed == true && vacation == false) ? true : false
+}
